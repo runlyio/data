88eebfb01fe3c5b5c000786c9d227b56f7bfaf81
willsoss <willsossamon@gmail.com>
2023-02-02 14:14:19 -0600

    Fix: Misc fixes

--- Runly.Data/Sql/DatabaseBuilder.cs
+++ Runly.Data/Sql/DatabaseBuilder.cs
@@ -8,7 +8,7 @@ using System.Text.RegularExpressions;
 
 namespace Runly.Data.Sql
 {
-    public class Database
+    public class DatabaseBuilder
     {
         const int azureGracePeriod = 3000;
         static readonly Regex goEx = new Regex(@"^\s*go\s*$", RegexOptions.Compiled | RegexOptions.Multiline | RegexOptions.IgnoreCase);
@@ -16,27 +16,18 @@ namespace Runly.Data.Sql
         readonly string connectionString;
         readonly int? commandTimeout;
 
-        readonly int schemaVersion;
         readonly string[] createSql;
         readonly string[] clearSql;
         readonly string[] populateSql;
 
-        readonly ILogger<Database> logger;
+        readonly ILogger<DatabaseBuilder> logger;
 
         public string ConnectionString => connectionString;
 
         /// <summary>
-        /// Initializes a new <see cref="Database"/> using the <paramref name="createScript"/>, <paramref name="clearScript"/>, and <paramref name="populateScript"/> provided.
+        /// Initializes a new <see cref="DatabaseBuilder"/> using the <paramref name="createScript"/>, <paramref name="clearScript"/>, and <paramref name="populateScript"/> provided.
         /// </summary>
-        /// <param name="connectionString"></param>
-        /// <param name="schemaVersion"></param>
-        /// <param name="createScript"></param>
-        /// <param name="clearScript"></param>
-        /// <param name="populateScript"></param>
-        /// <param name="logger"></param>
-        /// <param name="commandTimeout"></param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public Database(string connectionString, int schemaVersion, string createScript, string clearScript, string populateScript, ILogger<Database> logger, int? commandTimeout = null)
+        public DatabaseBuilder(string connectionString, string createScript, string clearScript, string populateScript, ILogger<DatabaseBuilder> logger, int? commandTimeout = null)
         {
             if (string.IsNullOrWhiteSpace(connectionString))
                 throw new ArgumentNullException(nameof(connectionString));
@@ -48,16 +39,15 @@ namespace Runly.Data.Sql
             this.commandTimeout = commandTimeout;
             this.logger = logger;
 
-            this.schemaVersion = schemaVersion;
             createSql = ParseScript(createScript);
             clearSql = ParseScript(clearScript ?? string.Empty);
             populateSql = ParseScript(populateScript ?? string.Empty);
         }
 
         /// <summary>
-        /// Initializes a <see cref="Database"/> using SQL scripts included as embedded resources found in the assembly and namespace of the <see cref="Type"/> <paramref name="namespace"/>. Expected script names are create.sql, clear.sql, and populate.sql.
+        /// Initializes a <see cref="DatabaseBuilder"/> using SQL scripts included as embedded resources found in the assembly and namespace of the <see cref="Type"/> <paramref name="namespace"/>. Expected script names are create.sql, clear.sql, and populate.sql.
         /// </summary>
-        public Database(string connectionString, int schemaVersion, Type @namespace, ILogger<Database> logger, int? commandTimeout = null)
+        public DatabaseBuilder(string connectionString, Type @namespace, ILogger<DatabaseBuilder> logger, int? commandTimeout = null)
         {
             if (string.IsNullOrWhiteSpace(connectionString))
                 throw new ArgumentNullException(nameof(connectionString));
@@ -69,7 +59,6 @@ namespace Runly.Data.Sql
             this.commandTimeout = commandTimeout;
             this.logger = logger;
 
-            this.schemaVersion = schemaVersion;
             createSql = ParseScript(ReadScriptFromEmbeddedResource(@namespace, "create"));
             clearSql = ParseScript(ReadScriptFromEmbeddedResource(@namespace, "clear"));
             populateSql = ParseScript(ReadScriptFromEmbeddedResource(@namespace, "populate"));
@@ -97,63 +86,6 @@ namespace Runly.Data.Sql
         string[] ParseScript(string script)
         {
             return goEx.Split(script).Where(c => !goEx.IsMatch(c) && !string.IsNullOrWhiteSpace(c)).ToArray();
-        }
-
-        public async Task MigrateAsync()
-        {
-            if (!await ExistsAsync())
-            {
-                // db doesn't exist; build it
-                await BuildAsync();
-                return;
-            }
-
-            int dbVersion = await GetSchemaVersionAsync();
-            if (dbVersion == schemaVersion)
-            {
-                logger.LogInformation($"Database schema version {dbVersion} is current.");
-                return; // all good; don't do anything
-            }
-            else
-            {
-                logger.LogInformation($"Database schema version {dbVersion} does is not current ({schemaVersion}).");
-            }
-
-            // need to blow the database away
-            await RebuildAsync();
-        }
-
-        async Task<int> GetSchemaVersionAsync()
-        {
-            int version = 0;
-
-            using (var conn = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    version = await conn.ExecuteScalarAsync<int>("select top 1 [version] from dbo.[dbschema] order by version desc;", commandTimeout: commandTimeout);
-                }
-                catch (SqlException) { /* if table doesn't exist, don't blow up, just return no version */ }
-            }
-
-            return version;
-        }
-
-        async Task<bool> ExistsAsync()
-        {
-            var builder = new SqlConnectionStringBuilder(connectionString);
-
-            string database = builder.InitialCatalog;
-            builder.InitialCatalog = "";
-
-            using (var conn = new SqlConnection(builder.ToString()))
-            {
-                bool exists = await conn.ExecuteScalarAsync<bool>("SELECT TOP 1 1 FROM sys.sysdatabases WHERE [Name] = @database", new { database }, commandTimeout: commandTimeout);
-
-                logger.LogInformation($"Database '{database}'{(exists ? " exists" : " does not exist")}.");
-
-                return exists;
-            }
         }
 
         public async Task RebuildAsync()
@@ -191,14 +123,6 @@ namespace Runly.Data.Sql
 
             using (var conn = new SqlConnection(connectionString))
             {
-                if (isAzure)
-                {
-                    logger.LogInformation($"Creating Azure logins/roles...");
-
-                    await conn.ExecuteAsync("CREATE USER [core-prod] FROM LOGIN [core-prod]");
-                    await conn.ExecuteAsync("EXEC sp_addrolemember 'db_owner', 'core-prod'");
-                }
-
                 await BuildSchemaAsync(conn);
             }
         }
@@ -216,8 +140,6 @@ namespace Runly.Data.Sql
 
             foreach (string statement in populateSql)
                 await ExecuteScriptAsync(conn, statement);
-
-            await conn.ExecuteAsync("insert into dbo.[dbschema] ([version]) values (@version)", new { schemaVersion });
         }
 
         public async Task ClearAsync()
